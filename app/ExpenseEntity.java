f8c1f672fc10ec4ad52c90e7e75ce4cb0213c7ba
DoQuocBinh <binhdq@fpt.edu.vn>
2024-11-04 12:46:23 +0700

    ok

---
+++ app/ExpenseEntity.java
@@ -0,0 +1,19 @@
+public class ExpenseEntity {
+    public int id;
+    public String expenseName;
+    public String expenseDate;
+    public String expenseType;
+    public String amount;
+
+    public ExpenseEntity() {
+
+    }
+
+    public ExpenseEntity(int id, String expenseName, String expenseDate, String expenseType, String amount) {
+        this.id = id;
+        this.expenseName = expenseName;
+        this.expenseDate = expenseDate;
+        this.expenseType = expenseType;
+        this.amount = amount;
+    }
+}
